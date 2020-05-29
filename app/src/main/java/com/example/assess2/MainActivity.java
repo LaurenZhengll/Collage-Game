220a80967061ff15bb9b45d354b29524fc68f35b
LaurenZhengll <LaurenZhengll@users.noreply.github.com>
2020-05-29 23:47:22 +1000

    Set close sticker by frame layout.

--- app/src/main/java/com/example/assess2/MainActivity.java
+++ app/src/main/java/com/example/assess2/MainActivity.java
@@ -6,14 +6,25 @@ import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.UUID;
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity implements ImageFragment.ImageFragmentListener {
 
@@ -26,6 +37,11 @@ public class MainActivity extends AppCompatActivity implements ImageFragment.Ima
     BrushFragment brushFragment;
     CardView imageBtn;
     ImageView sticker;
+    ImageView backgroundImg;
+    ViewGroup viewGroup = null;
+
+    private LayoutInflater inflater;
+
 
 
     @Override
@@ -39,6 +55,10 @@ public class MainActivity extends AppCompatActivity implements ImageFragment.Ima
         //getSupportFragmentManager().beginTransaction().add(R.id.frag_container, new BrushFragment()).commit();
 
         imageBtn = findViewById(R.id.btnImage);
+        backgroundImg = findViewById(R.id.imageView);
+
+        inflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
 
     }
 
@@ -80,9 +100,6 @@ public class MainActivity extends AppCompatActivity implements ImageFragment.Ima
 
     }
 
-    private void loadImage(){
-
-    }
 
     public void onClickBrushBtn(View view){ // click brush button
         brushFragment = new BrushFragment();
@@ -92,7 +109,7 @@ public class MainActivity extends AppCompatActivity implements ImageFragment.Ima
         getSupportFragmentManager().beginTransaction().replace(R.id.frag_container, brushFragment).commit();
     }
 
-    @Override
+   /* @Override
     public void onImageItemClick(int position) {
         sticker = findViewById(R.id.sticker);
         if (position == 0) {
@@ -102,5 +119,72 @@ public class MainActivity extends AppCompatActivity implements ImageFragment.Ima
         } else {
             sticker.getDrawable().setLevel(3);
         }
+        addViewToParent(sticker);
+    }*/
+
+    @Override
+    public void onImageItemClick(int position) {
+
+        //View imageRootView = inflater.inflate(R.layout.view_add_image, null);
+        ImageView imageContent = findViewById(R.id.imageContent);
+        //final FrameLayout frmBorder = imageRootView.findViewById(R.id.frmBorder);
+        //final ImageView icClose = imageRootView.findViewById(R.id.icClose);
+
+        if (position == 0) {
+            imageContent.getDrawable().setLevel(1);
+        } else if (position == 1) {
+            imageContent.getDrawable().setLevel(2);
+        } else {
+            imageContent.getDrawable().setLevel(3);
+        }
+
+
+      /*  MultiTouchListener multiTouchListener = getMultiTouchListener();
+        multiTouchListener.setOnGestureControl(new MultiTouchListener.OnGestureControl() {
+            @Override
+            public void onClick() {
+                boolean isBackgroundVisible = frmBorder.getTag() != null && (boolean) frmBorder.getTag();
+                frmBorder.setBackgroundResource(isBackgroundVisible ? 0 : R.drawable.rounded_border_tv);
+                imgClose.setVisibility(isBackgroundVisible ? View.GONE : View.VISIBLE);
+                frmBorder.setTag(!isBackgroundVisible);
+            }
+
+            @Override
+            public void onLongClick() {
+
+            }
+        });
+
+        imageRootView.setOnTouchListener(multiTouchListener);
+
+        addViewToParent(imageRootView);*/
+
     }
+
+    /*public void save(View view){
+        drawingView.setDrawingCacheEnabled(true);
+        String imagSaved = MediaStore.Images.Media.insertImage(getContentResolver(),drawingView.getDrawingCache(), UUID.randomUUID().toString()+".png","drawing");
+        if(imagSaved!=null){
+            Toast savedToast = Toast.makeText(getApplicationContext(),"Image saved",Toast.LENGTH_LONG);
+            savedToast.show();
+        } else {
+            Toast unsavedToast = Toast.makeText(getApplicationContext(),"Image not saved",Toast.LENGTH_LONG);
+            unsavedToast.show();
+        }
+        drawingView.destroyDrawingCache();
+
+    }
+*/
+    /*private void addViewToParent(View rootView) {
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+
+        viewGroup.addView(backgroundImg);
+        viewGroup.addView(rootView,params);
+
+        //addedViews.add(rootView);
+        *//*if (mOnPhotoEditorListener != null)
+            mOnPhotoEditorListener.onAddViewListener(viewType, addedViews.size());*//*
+    }*/
 }
